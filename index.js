d29f077a8bdfd3b8fb0659b937ca495d35587d27
Daryl Norrell <cheyannewascom@gmail.com>
2022-07-23 21:29:17 -0700

    commit tree 1d72f7d230c18cf659765f9b6e3a258c1ccd574c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.push("Broom");
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const newCopyOfCats = cats.slice();
+    newCopyOfCats.unshift("Arnold");
+    return newCopyOfCats;
+}
+
+function removeLastCat() {
+    const newCatsArray = cats.slice();
+    newCatsArray.pop();
+    return newCatsArray;
+}
+
+function removeFirstCat() {
+    const newNewCatsArray = cats.slice();
+    newNewCatsArray.shift();
+    return newNewCatsArray;
+}
